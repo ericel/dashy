f47af358071339c0ef8f60f54915eb88722e989c
Oj Obasi <ericel123new@gmail.com>
2016-07-22 11:25:32 +0900

    google charts

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,7 @@ angular
     'ngTouch',
     'ui.router',
     'ui.router.title',
-    //'googlechart',
+    'googlechart',
     //'uiGmapgoogle-maps',
     'easypiechart',
     'chart.js',
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -50,6 +50,7 @@ module.exports = function(config) {
       'bower_components/angular-datatables/dist/plugins/tabletools/angular-datatables.tabletools.js',
       'bower_components/angular-datatables/dist/plugins/buttons/angular-datatables.buttons.js',
       'bower_components/angular-datatables/dist/plugins/select/angular-datatables.select.js',
+      'bower_components/angular-google-chart/ng-google-chart.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
